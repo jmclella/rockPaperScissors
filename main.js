2981398d2f97238c236be67d883f103450fcf8e7
jmclella <jmclella@fandm.edu>
2022-11-19 12:55:15 -0500

    Added javascript playGame after button click
    Determins Score variables and choice of player
    and runs the playRound function and getComputerChoice
    function to determine random winner.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // JavaScript program for Rock, Paper, Scissors
 
+
 // Declare Global Variables
 let items = ['rock', 'paper', 'scissors'];
-
 
 // The getComputerChoice function gets random choice between rock/paper/scissors for computer
 
@@ -10,6 +10,26 @@ function getComputerChoice() {
     var choice = items[Math.floor(Math.random()*items.length)];
     return choice;
 }
+
+// Declare button variables
+const buttons = document.querySelectorAll('#select');
+
+
+// Every time a player choice is selected - run computerChoice() and game
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerChoice = (button.parentElement.getAttribute('class'));
+        computerChoice = getComputerChoice();
+        playRound(playerChoice, computerChoice);
+    })
+})
+
+let playerScoreContainer = document.querySelector('#player-score');
+let playerScore = playerScoreContainer.textContent;
+playerScore = parseInt(playerScore);
+let computerScoreContainer = document.querySelector('#computer-score');
+let computerScore = playerScoreContainer.textContent;
+computerScore = parseInt(computerScore);
 
 // Create playRound function with (playerChoice, computerChoice) parameters
 
@@ -54,51 +74,3 @@ function playRound(playerChoice, computerChoice) {
     console.log(`You ${result}!`);
     return result;
 }
-
-
-// Create game() function that uses for loop to playRound 5 times, keeps score, and reports winner at end of game
-
-function game() {
-
-    // Declare Player and Computer Scores
-    var playerScore = 0;
-    var computerScore = 0;
-
-    // Loop through 5 games and add scores
-    for (let i = 0; i < 5; i++) {
-        // Prompt user to make player choice
-        // let playerChoice = (prompt('Choose Rock, Paper, or Scissors')).toLowerCase();
-        // Call getComputerChoice function to get computer choice
-        let computerChoice = getComputerChoice();
-
-        // Call result variable from playRound function
-        let result = playRound(playerChoice, computerChoice);
-        // Add to player/computer score depending on result
-        if (result === 'win') {
-            playerScore += 1;
-        }
-        else if (result === 'lose') {
-            computerScore += 1;
-        }
-        
-        // Print scores
-        console.log(`Your score is: ${playerScore}`);
-        console.log(`Computer score is: ${computerScore}`);
-    }
-
-    // Determine winner of the game
-    if (playerScore > computerScore) {
-        console.log(`You win the game! Score: ${playerScore} - ${computerScore}`);
-    }
-    else if (computerScore > playerScore) {
-        console.log(`You lose the game! Score: ${playerScore} - ${computerScore}`);
-    }
-    else {
-        console.log(`It's a draw! Score: ${playerScore} - ${computerScore}`);
-    }
-    
-    return playerScore, computerScore;
-}
-
-// Play game
-console.log(game())
